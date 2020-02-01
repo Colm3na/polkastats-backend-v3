4af9487f68a7eb36b3980f4ef39b8fdb1e5ebd2c
mariopino <info@quequiereshacer.es>
2020-02-01 10:08:33 +0100

    Fix sql

--- crawlers/blocks.js
+++ crawlers/blocks.js
@@ -63,7 +63,7 @@ async function main () {
       const pool = new Pool(postgresConnParams);
 
       var sqlInsert =
-        'INSERT INTO block (block_number, block_finalized, block_author, block_hash, parent_hash, extrinsics_root, state_root, total_issuance, session_json, timestamp) VALUES (\'' + blockNumber + '\', \'' + blockFinalized + '\', \'' + extendedHeader.author + '\', \'' + blockHash + '\', \'' + parentHash + '\', \'' + extrinsicsRoot + '\', \'' + stateRoot + '\', \'' + totalIssuance + '\', \'' + JSON.stringify(session) + '\', UNIX_TIMESTAMP());';
+        'INSERT INTO block (block_number, block_finalized, block_author, block_hash, parent_hash, extrinsics_root, state_root, total_issuance, session_json, timestamp) VALUES (\'' + blockNumber + '\', \'' + blockFinalized + '\', \'' + extendedHeader.author + '\', \'' + blockHash + '\', \'' + parentHash + '\', \'' + extrinsicsRoot + '\', \'' + stateRoot + '\', \'' + totalIssuance + '\', \'' + JSON.stringify(session) + '\', extract(epoch from now());';
       const res = await pool.query(sqlInsert)
       
       // We connect/disconnect to MySQL in each loop to avoid problems if MySQL server is restarted while the crawler is running
