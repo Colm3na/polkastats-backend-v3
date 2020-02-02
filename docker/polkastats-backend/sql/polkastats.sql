a3ce01ffe259a5abd87a649ead2f67e6cc3a2c60
derfredy <derfredy@gmail.com>
2020-02-02 11:00:25 +0100

    remove mariadb container

---
+++ docker/polkastats-backend/sql/polkastats.sql
@@ -0,0 +1,23 @@
+CREATE USER polkastats WITH PASSWORD 'polkastats';
+GRANT ALL PRIVILEGES ON DATABASE polkastats TO polkastats;
+GRANT ALL PRIVILEGES ON TABLE block TO polkastats;
+CREATE TABLE block (  
+   block_number BIGINT NOT NULL,
+   block_author VARCHAR(47) NOT NULL,
+   block_author_name VARCHAR(100) NOT NULL,
+   block_hash VARCHAR(66) NOT NULL,
+   parent_hash VARCHAR(66) NOT NULL,
+   extrinsics_root VARCHAR(66) NOT NULL,
+   state_root VARCHAR(66) NOT NULL,
+   current_era BIGINT NOT NULL,
+   current_index BIGINT NOT NULL,
+   era_length BIGINT NOT NULL,
+   era_progress BIGINT NOT NULL,
+   is_epoch BOOLEAN NOT NULL,
+   session_length BIGINT NOT NULL,
+   session_per_era INT NOT NULL,
+   session_progress BIGINT NOT NULL,
+   validator_count INT NOT NULL,
+   timestamp BIGINT NOT NULL,
+   PRIMARY KEY ( block_number )  
+);
