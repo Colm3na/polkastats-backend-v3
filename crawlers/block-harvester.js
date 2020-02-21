55cba3b71c710bb462ec0e825e9805221b6e999d
mariopino <info@quequiereshacer.es>
2020-02-21 23:07:55 +0100

    Improve harvester

--- crawlers/block-harvester.js
+++ crawlers/block-harvester.js
@@ -91,169 +91,150 @@ async function main () {
     // Log execution time
     //
     console.log(`[PolkaStats backend v3] - Block harvester - \x1b[32mAdded ${addedBlocks} blocks in ${((endTime - startTime) / 1000).toFixed(0)}s\x1b[0m`);
+    console.log(`[PolkaStats backend v3] - Block harvester - \x1b[32mNext execution in 60m...\x1b[0m`);
+    setTimeout(main, 60 * 60 * 1000);
+  } else {
+    console.log(`[PolkaStats backend v3] - Block harvester - \x1b[31mNode is not synced! Waiting 10s...\x1b[0m`);
+    setTimeout(main, 10000);
   }
+}
 
-  async function harvestBlocks(api, startBlock, endBlock) {
-
-    while (startBlock <= endBlock) {
-
-      // Start execution
-      const startTime = new Date().getTime();
-
-      // Get block hash
-      const blockHash = await api.rpc.chain.getBlockHash(startBlock);
-
-      // Get extended block header
-      const extendedHeader = await api.derive.chain.getHeader(blockHash);
-
-      // Get block parent hash
-      const parentHash = extendedHeader.parentHash;
+async function harvestBlocks(api, startBlock, endBlock) {
+  while (startBlock <= endBlock) {
+    // Start execution
+    const startTime = new Date().getTime();
+    // Get block hash
+    const blockHash = await api.rpc.chain.getBlockHash(startBlock);
+    // Get extended block header
+    const extendedHeader = await api.derive.chain.getHeader(blockHash);
+    // Get block parent hash
+    const parentHash = extendedHeader.parentHash;
+    
+    // Get block extrinsics root
+    const extrinsicsRoot = extendedHeader.extrinsicsRoot;
+    // Get block state root
+    const stateRoot = extendedHeader.stateRoot;
+    // Get block events
+    const blockEvents = await api.query.system.events.at(blockHash);
+    // Loop through the Vec<EventRecord>
+    blockEvents.forEach( async (record, index) => {
       
-      // Get block extrinsics root
-      const extrinsicsRoot = extendedHeader.extrinsicsRoot;
-
-      // Get block state root
-      const stateRoot = extendedHeader.stateRoot;
-
-      // Get block events
-      const blockEvents = await api.query.system.events.at(blockHash);
-
-      // Loop through the Vec<EventRecord>
-      blockEvents.forEach( async (record, index) => {
-        
-        // Extract the phase and event
-        const { event, phase } = record;
-
-        console.log(JSON.stringify(record, null, 2));
-
-        //
-        //  TODO: Update counters in block table:
-        //
-        //  total_extrinsics
-        //  total_signed_extrinsics
-        //  total_failed_extrinsics
-        //  total_events
-        //  total_system_events
-        //  total_module_events
-        //  new_accounts
-        //  reaped_accounts
-        //  new_contracts
-        //  new_sessions
-        //
-
-        const sqlInsert = 
-          `INSERT INTO event (
-            block_number,
-            event_index,
-            section,
-            method,
-            phase,
-            data
-          ) VALUES (
-            '${startBlock}',
-            '${index}',
-            '${event.section}',
-            '${event.method}',
-            '${phase.toString()}',
-            '${JSON.stringify(event.data)}'
-          );`;
-        try {
-          await pool.query(sqlInsert);
-          console.log(`[PolkaStats backend v3] - Block harvester - \x1b[33mAdding event #${startBlock}-${index} ${event.section} => ${event.method}\x1b[0m`);
-        } catch (error) {
-          console.log(`[PolkaStats backend v3] - Block harvester - \x1b[31mError adding event #${startBlock}-${index}: ${error.error}\x1b[0m`);
-        }
-      });
-
-      // Get session info for the block
-      const currentIndex = await api.query.session.currentIndex.at(blockHash);
-      const currentSlot = await api.query.babe.currentSlot.at(blockHash);
-      const epochIndex = await api.query.babe.epochIndex.at(blockHash);
-      const genesisSlot = await api.query.babe.genesisSlot.at(blockHash);
-      const currentEraStartSessionIndex = await api.query.staking.currentEraStartSessionIndex.at(blockHash);
-      const currentEra = await api.query.staking.currentEra.at(blockHash);
-      const validatorCount = await api.query.staking.validatorCount.at(blockHash);
-
-      const epochDuration = api.consts.babe.epochDuration;
-      const sessionsPerEra = api.consts.staking.sessionsPerEra;
-      const eraLength = epochDuration.mul(sessionsPerEra);
-
-      const epochStartSlot = epochIndex.mul(epochDuration).add(genesisSlot);
-      const sessionProgress = currentSlot.sub(epochStartSlot);
-      const eraProgress = currentIndex.sub(currentEraStartSessionIndex).mul(epochDuration).add(sessionProgress);
-      
-      // Get block author
-      const blockAuthor = extendedHeader.author;
-
-      // Get block author identity display name
-      const blockAuthorIdentity = await api.derive.accounts.info(blockAuthor);
-      const blockAuthorName = blockAuthorIdentity.identity.display || ``;
-
-      // Get runtime spec name and version
-      const runtimeVersion = await api.rpc.state.getRuntimeVersion(blockHash);
-
-      // TODO: Get timestamp from block
-      const timestamp = new Date().getTime();
-
-      const sqlInsert =
-        `INSERT INTO block (
+      // Extract the phase and event
+      const { event, phase } = record;
+      console.log(JSON.stringify(record, null, 2));
+      //
+      //  TODO: Update counters in block table:
+      //
+      //  total_extrinsics
+      //  total_signed_extrinsics
+      //  total_failed_extrinsics
+      //  total_events
+      //  total_system_events
+      //  total_module_events
+      //  new_accounts
+      //  reaped_accounts
+      //  new_contracts
+      //  new_sessions
+      //
+      const sqlInsert = 
+        `INSERT INTO event (
           block_number,
-          block_author,
-          block_author_name,
-          block_hash,
-          parent_hash,
-          extrinsics_root,
-          state_root,
-          current_era,
-          current_index,
-          era_length,
-          era_progress,
-          is_epoch,
-          session_length,
-          session_per_era,
-          session_progress,
-          validator_count,
-          spec_name,
-          spec_version,
-          timestamp
+          event_index,
+          section,
+          method,
+          phase,
+          data
         ) VALUES (
           '${startBlock}',
-          '${blockAuthor}',
-          '${blockAuthorName}',
-          '${blockHash}',
-          '${parentHash}',
-          '${extrinsicsRoot}',
-          '${stateRoot}',
-          '${currentEra}',
-          '${currentIndex}',
-          '${eraLength}',
-          '${eraProgress}',
-          'true',
-          '${epochDuration}',
-          '${sessionsPerEra}',
-          '${sessionProgress}',
-          '${validatorCount}',
-          '${runtimeVersion.specName}',
-          '${runtimeVersion.specVersion}',
-          '${timestamp}'
-        )`;
+          '${index}',
+          '${event.section}',
+          '${event.method}',
+          '${phase.toString()}',
+          '${JSON.stringify(event.data)}'
+        );`;
       try {
         await pool.query(sqlInsert);
-        const endTime = new Date().getTime();
-        console.log(`[PolkaStats backend v3] - Block harvester - \x1b[32mAdded block #${startBlock} (${shortHash(blockHash.toString())}) in ${((endTime - startTime) / 1000).toFixed(3)}s\x1b[0m`);
+        console.log(`[PolkaStats backend v3] - Block harvester - \x1b[33mAdding event #${startBlock}-${index} ${event.section} => ${event.method}\x1b[0m`);
       } catch (error) {
-        console.log(`[PolkaStats backend v3] - Block harvester - \x1b[31mError adding block #${startBlock}: ${error.error}\x1b[0m`);
+        console.log(`[PolkaStats backend v3] - Block harvester - \x1b[31mError adding event #${startBlock}-${index}: ${error.error}\x1b[0m`);
       }
-      startBlock++;
-      addedBlocks++;
+    });
+    // Get session info for the block
+    const currentIndex = await api.query.session.currentIndex.at(blockHash);
+    const currentSlot = await api.query.babe.currentSlot.at(blockHash);
+    const epochIndex = await api.query.babe.epochIndex.at(blockHash);
+    const genesisSlot = await api.query.babe.genesisSlot.at(blockHash);
+    const currentEraStartSessionIndex = await api.query.staking.currentEraStartSessionIndex.at(blockHash);
+    const currentEra = await api.query.staking.currentEra.at(blockHash);
+    const validatorCount = await api.query.staking.validatorCount.at(blockHash);
+    const epochDuration = api.consts.babe.epochDuration;
+    const sessionsPerEra = api.consts.staking.sessionsPerEra;
+    const eraLength = epochDuration.mul(sessionsPerEra);
+    const epochStartSlot = epochIndex.mul(epochDuration).add(genesisSlot);
+    const sessionProgress = currentSlot.sub(epochStartSlot);
+    const eraProgress = currentIndex.sub(currentEraStartSessionIndex).mul(epochDuration).add(sessionProgress);
+    
+    // Get block author
+    const blockAuthor = extendedHeader.author;
+    // Get block author identity display name
+    const blockAuthorIdentity = await api.derive.accounts.info(blockAuthor);
+    const blockAuthorName = blockAuthorIdentity.identity.display || ``;
+    // Get runtime spec name and version
+    const runtimeVersion = await api.rpc.state.getRuntimeVersion(blockHash);
+    // TODO: Get timestamp from block
+    const timestamp = new Date().getTime();
+    const sqlInsert =
+      `INSERT INTO block (
+        block_number,
+        block_author,
+        block_author_name,
+        block_hash,
+        parent_hash,
+        extrinsics_root,
+        state_root,
+        current_era,
+        current_index,
+        era_length,
+        era_progress,
+        is_epoch,
+        session_length,
+        session_per_era,
+        session_progress,
+        validator_count,
+        spec_name,
+        spec_version,
+        timestamp
+      ) VALUES (
+        '${startBlock}',
+        '${blockAuthor}',
+        '${blockAuthorName}',
+        '${blockHash}',
+        '${parentHash}',
+        '${extrinsicsRoot}',
+        '${stateRoot}',
+        '${currentEra}',
+        '${currentIndex}',
+        '${eraLength}',
+        '${eraProgress}',
+        'true',
+        '${epochDuration}',
+        '${sessionsPerEra}',
+        '${sessionProgress}',
+        '${validatorCount}',
+        '${runtimeVersion.specName}',
+        '${runtimeVersion.specVersion}',
+        '${timestamp}'
+      )`;
+    try {
+      await pool.query(sqlInsert);
+      const endTime = new Date().getTime();
+      console.log(`[PolkaStats backend v3] - Block harvester - \x1b[32mAdded block #${startBlock} (${shortHash(blockHash.toString())}) in ${((endTime - startTime) / 1000).toFixed(3)}s\x1b[0m`);
+    } catch (error) {
+      console.log(`[PolkaStats backend v3] - Block harvester - \x1b[31mError adding block #${startBlock}: ${error.error}\x1b[0m`);
     }
+    startBlock++;
+    addedBlocks++;
   }
-  console.log(`[PolkaStats backend v3] - Block harvester - \x1b[31mNode is not synced! Waiting 10s...\x1b[0m`);
-  setTimeout(function() {
-    if (!isSynced) {
-      main();
-    }
-  }, 10000);
 }
 
 main().catch((error) => {
