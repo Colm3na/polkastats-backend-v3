b039b6b7f58d56038c6a4cbe5fba4caeefe38b22
mariopino <info@quequiereshacer.es>
2020-03-08 14:53:07 +0100

    testing single point entry

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const config = require('../backend.config.js');
+const BackendV3 = require('lib/BackendV3.js');
+
+async function main () {
+  const backendV3 = new BackendV3(config); 
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(-1);
+});
