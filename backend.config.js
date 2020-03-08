cb286e363d7e7694a099f3e45ea6e16c625c1f57
mariopino <info@quequiereshacer.es>
2020-03-08 16:50:36 +0100

    split phragmen crawler

--- backend.config.js
+++ backend.config.js
@@ -11,6 +11,8 @@ module.exports = {
     password: 'polkastats',
     port: 5432,
   },
+  offlinePhragmenPath: `/usr/app/offline-phragmen/offline-phragmen`,
   BLOCK_HARVESTER_POLLING_TIME: 1 * 60 * 1000, // Run every 10 min
-  ACTIVE_ACCOUNTS_POLLING_TIME: 1 * 60 * 1000 // Run every 60 min
+  ACTIVE_ACCOUNTS_POLLING_TIME: 1 * 60 * 1000, // Run every 60 min
+  PHRAGMEN_POLLING_TIME: 1 * 60 * 1000, // Run every 5 min
 }
