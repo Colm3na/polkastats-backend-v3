690861008d814775c1ff2fb4ece12064b96e43a8
Aitor Ibañez <aitiba@gmail.com>
2020-03-09 17:01:38 +0000

    Add new service and fix some paths

--- backend.config.js
+++ backend.config.js
@@ -9,7 +9,7 @@ module.exports = {
     password: 'polkastats',
     port: 5432,
   },
-  offlinePhragmenPath: `/usr/app/offline-phragmen/offline-phragmen`,
+  offlinePhragmenPath: `/app/offline-phragmen`,
   BLOCK_HARVESTER_POLLING_TIME: 1 * 60 * 1000, // Run every 10 min
   ACTIVE_ACCOUNTS_POLLING_TIME: 1 * 60 * 1000, // Run every 60 min
   PHRAGMEN_POLLING_TIME: 1 * 60 * 1000, // Run every 5 min
