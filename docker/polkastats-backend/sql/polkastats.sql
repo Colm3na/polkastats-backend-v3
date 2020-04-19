54b1b9cb905884628e6ad428c3a81e3a21484515
mariopino <info@quequiereshacer.es>
2020-04-19 19:00:58 +0200

    fix typo

--- docker/polkastats-backend/sql/polkastats.sql
+++ docker/polkastats-backend/sql/polkastats.sql
@@ -139,7 +139,7 @@ CREATE TABLE IF NOT EXISTS system  (
 CREATE TABLE IF NOT EXISTS chain  (
   block_height BIGINT NOT NULL,
   session_index INT NOT NULL,
-  total_issuance BIGINT NOT NULL,,
+  total_issuance BIGINT NOT NULL,
   active_accounts BIGINT NOT NULL,
   timestamp BIGINT NOT NULL,
   PRIMARY KEY ( block_height )  
