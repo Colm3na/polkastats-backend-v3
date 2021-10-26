97e54bfda6321a0ea1269bf6fb68cc3753540c4a
Anton Drelin <antondrelin@gmail.com>
2021-10-26 14:22:06 +0300

    Updated sequelizerc;

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -8,7 +8,17 @@ module.exports = {
     database: process.env.POSTGRES_DATABASE,
     port: 5432,
     host: '127.0.0.1',  
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: true
+  },
+  stage: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    port: 5432,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    logging: false
   },
   production: {
     username: process.env.POSTGRES_USER,
