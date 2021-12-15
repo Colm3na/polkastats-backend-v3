af583abffc495370a69c097bb997a062e42d350a
Anton Drelin <antondrelin@gmail.com>
2021-12-15 14:33:54 +0300

    fix: amount

--- sequelize/migrations/20211214115604-fixed_account.js
+++ sequelize/migrations/20211214115604-fixed_account.js
@@ -19,17 +19,18 @@ module.exports = {
        await queryInterface.addColumn('extrinsic', 'timestamp', {
         type: Sequelize.DataTypes.BIGINT
        }, { transaction });
-
-       await queryInterface.removeColumn('event', 'amount', 
-       {
-         transaction
-       });
-      
+             
        await queryInterface.removeColumn('extrinsic', 'doc', { transaction });
 
        await queryInterface.addColumn('extrinsic', 'amount', {
         type: Sequelize.DataTypes.BIGINT,
         defaultValue: 0
+       }, { transaction });
+
+       await queryInterface.addColumn('event', 'amount', 
+       {
+        type: Sequelize.DataTypes.BIGINT,
+        defaultValue: 0,        
        }, { transaction });
 
        await queryInterface.addColumn('extrinsic', 'fee', {
