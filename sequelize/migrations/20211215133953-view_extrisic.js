ce0c7d89eb8cb898b389918955ed64c4460f8038
Anton Drelin <antondrelin@gmail.com>
2021-12-15 16:58:36 +0300

    fix: Updated view view_extrinsic

---
+++ sequelize/migrations/20211215133953-view_extrisic.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     const transaction = await queryInterface.sequelize.transaction()
+     try {
+      await queryInterface.sequelize.query('drop view view_extrinsic', {
+        transaction
+      });
+
+      await queryInterface.sequelize.query(`
+      create view view_extrinsic as 
+        select 
+        concat(block_number,'-', extrinsic_index) as block_index,
+        block_number,
+        signer as from_owner,
+        (cast(args as json)::json->0)::json->>'id' as to_owner,
+        hash,
+        success,
+        timestamp,
+        fee,
+        amount from extrinsic
+      where 
+      method in ('transfer', 'transferAll', 'transferKeepAlive', 'vestedTransfer')`, {
+        transaction
+      });
+      await transaction.commit();
+     } catch (err) {
+      await transaction.rollback();
+      throw err;
+     }
+    //
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
