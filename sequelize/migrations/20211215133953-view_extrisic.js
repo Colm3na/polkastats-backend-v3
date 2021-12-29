8a261b54896130efcf6cde0c2f59efd78c3b738f
Anton Drelin <antondrelin@gmail.com>
2021-12-29 20:43:45 +0300

    fix: Added fee for extrisic
    Jira issue: [NFTPAR-1046]

--- sequelize/migrations/20211215133953-view_extrisic.js
+++ sequelize/migrations/20211215133953-view_extrisic.js
@@ -10,10 +10,7 @@ module.exports = {
      */
      const transaction = await queryInterface.sequelize.transaction()
      try {
-      await queryInterface.sequelize.query('drop view view_extrinsic', {
-        transaction
-      });
-
+       
       await queryInterface.changeColumn('event', 'amount', 
       {
        type: Sequelize.DataTypes.TEXT,       
@@ -26,26 +23,7 @@ module.exports = {
       await queryInterface.changeColumn('extrinsic', 'amount', {
         type: Sequelize.DataTypes.TEXT        
       }, { transaction });
-
-      
-      await queryInterface.sequelize.query(`
-      create view view_extrinsic as 
-      select
-      concat(block_number,'-', extrinsic_index) as block_index,
-      block_number,
-      signer as from_owner,
-      coalesce((cast(args as json)::json->0)::json->>'id', (cast(args as json)::json->0)::json->>'substrate') as to_owner,
-      hash,
-      success,
-      timestamp,
-      fee,
-      method,
-      section,
-      amount from extrinsic
-    where
-    method in ('transfer', 'transferAll', 'transferKeepAlive', 'vestedTransfer')`, {
-        transaction
-      });
+    
       await transaction.commit();
      } catch (err) {
       await transaction.rollback();
