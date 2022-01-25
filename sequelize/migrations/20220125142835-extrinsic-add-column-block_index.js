58a4941464708d206add1c4642f89f4fb61e2eb6
Sergey Spitsyn <sswebcoder@gmail.com>
2022-01-25 21:10:25 +0300

    Transaction for adding column and index for column was removed, because can't be completed.

--- sequelize/migrations/20220125142835-extrinsic-add-column-block_index.js
+++ sequelize/migrations/20220125142835-extrinsic-add-column-block_index.js
@@ -2,23 +2,13 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const transaction = await queryInterface.sequelize.transaction()
-    
-    try {
-      await queryInterface.addColumn('extrinsic', 'block_index', {
-        type: Sequelize.DataTypes.STRING,
-      });
+    await queryInterface.addColumn('extrinsic', 'block_index', {
+      type: Sequelize.DataTypes.STRING,
+    });
 
-      await queryInterface.addIndex('extrinsic', ['block_index'], {
-        name: 'extrinsic_block_index_idx'
-      });
-
-      await transaction.commit();
-    } catch (err) {
-      console.log('err->', err);
-      await transaction.rollback();
-      throw err;
-    }    
+    await queryInterface.addIndex('extrinsic', ['block_index'], {
+      name: 'extrinsic_block_index_idx'
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
