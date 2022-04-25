e93de0812b6473b2ec2a9fec3c8c72698ca6ed6a
Sergey Spitsyn <sswebcoder@gmail.com>
2022-04-25 21:14:03 +0300

    Check token events before save tokens.

--- lib/eventFactory/token/createToken.ts
+++ lib/eventFactory/token/createToken.ts
@@ -1,11 +1,37 @@
+import eventsDB from '../../../lib/eventsDB';
 import tokenDB from '../../../lib/tokenDB';
 import { EventToken } from '../eventToken';
+import { EventTypes } from '../type';
 
 export class CreateToken extends EventToken {
   async save() {
-    if (this.collectionId && this.tokenId) {
+    if (!this.collectionId || !this.tokenId) {
+      throw new Error(`Can't create token without collectionId(${this.collectionId}) or tokenId(${this.tokenId})`);
+    }
+
+    const canCreateNewToken = await this.canCreateToken();
+
+    if (canCreateNewToken) {
       const token = await this.getToken();
       await tokenDB.save(token, this.sequelize);
     }
-  }  
+  }
+
+  private async canCreateToken(): Promise<boolean> {
+    const destroyTokenEvent = await eventsDB.getTokenEvent(
+      this.sequelize,
+      this.collectionId,
+      this.tokenId,
+      EventTypes.TYPE_ITEM_DESTROYED,
+    );
+
+    const destroyCollectionEvent = await eventsDB.getTokenEvent(
+      this.sequelize,
+      this.collectionId,
+      this.tokenId,
+      EventTypes.TYPE_COLLECTION_DESTROYED,
+    );
+
+    return !destroyTokenEvent && !destroyCollectionEvent;
+  }
 }
