06959621f472ad68d87cfa6898420a42a082097d
Sergey Spitsyn <sswebcoder@gmail.com>
2022-05-04 09:53:13 +0300

    New column account_id_normalized for store normalized account id in account table.

--- sequelize/migrations/20220329064622-account-normalize-account_id.js
+++ sequelize/migrations/20220329064622-account-normalize-account_id.js
@@ -2,30 +2,72 @@
 
 const { normalizeSubstrateAddress } = require('../../utils/utils');
 
+async function normalizeAddress(queryInterface, Sequelize, transaction) {
+  const accounts = await queryInterface.sequelize.query('select account_id from account where account_id_normalized is null limit 1000', {
+    type: Sequelize.QueryTypes.SELECT,
+    logging: false,
+    plain: false,
+    transaction,
+  });
+
+  if (accounts.length === 0) {
+    return;
+  }
+
+  for (const account of accounts) {
+    await queryInterface.sequelize.query(
+      `update account set account_id_normalized = :normalizedAddress where account_id = :account_id`,
+      {
+        type: Sequelize.QueryTypes.UPDATE,
+        logging: false,
+        replacements: {
+          account_id: account.account_id,
+          normalizedAddress: normalizeSubstrateAddress(account.account_id),
+        },
+        transaction,
+      },
+    );
+  }
+
+  await normalizeAddress(queryInterface, Sequelize, transaction);
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const transaction = await queryInterface.sequelize.transaction();
 
     try {
-      const accounts = await queryInterface.sequelize.query('select account_id from account', {
-        type: Sequelize.QueryTypes.SELECT,
-        logging: false,
+      await queryInterface.addColumn(
+        'account',
+        'account_id_normalized',
+        {
+          type: Sequelize.DataTypes.TEXT,
+          allowNull: true,
+        },
+        {
+          transaction,
+        },
+      );
+
+      await normalizeAddress(queryInterface, Sequelize, transaction);
+
+      await queryInterface.changeColumn(
+        'account',
+        'account_id_normalized',
+        {
+          type: Sequelize.DataTypes.TEXT,
+          allowNull: false,
+        },
+        {
+          transaction,
+        },
+      );
+
+      await queryInterface.addIndex('account', ['account_id_normalized'], {
+        name: 'account_account_id_normalized_idx',
         transaction,
       });
 
-      for (const account of accounts) {
-        await queryInterface.sequelize.query(
-          `update account set account_id = :normalizedAddress where account_id = :account_id`,
-          {
-            type: Sequelize.QueryTypes.UPDATE,
-            logging: false,
-            replacements: {
-              account_id: account.account_id,
-              normalizedAddress: normalizeSubstrateAddress(account.account_id),
-            },
-            transaction,
-          });
-      }
       await transaction.commit();
     } catch (err) {
       console.error(err);
@@ -34,5 +76,7 @@ module.exports = {
     }
   },
 
-  down: async (queryInterface, Sequelize) => { }
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('account', 'account_id_normalized');
+  }
 };
