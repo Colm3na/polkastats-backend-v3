5f69c4efef10d0eba68424947ec156f791502fd4
Sergey Spitsyn <sswebcoder@gmail.com>
2022-05-16 09:26:18 +0300

    view_tokens owner_normalized column was added.

--- sequelize/migrations/20220516060536-view_holders-add-owner_normalized.js
+++ sequelize/migrations/20220516060536-view_holders-add-owner_normalized.js
@@ -17,15 +17,18 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     const transaction = await queryInterface.sequelize.transaction();
     try {
-      await queryInterface.sequelize.query('DROP VIEW view_holders;', transaction);
-      await queryInterface.sequelize.query(`
+      await queryInterface.sequelize.query('DROP VIEW view_holders;', { transaction });
+      await queryInterface.sequelize.query(
+        `
         CREATE OR REPLACE VIEW public.view_holders
         AS SELECT t.collection_id,
           count(t.token_id) AS count,
           t.owner
         FROM tokens t
         GROUP BY t.collection_id, t.owner;
-    `);
+      `,
+      { transaction },
+      );
       await transaction.commit();
     } catch (err) {
       console.error(err);
