d5cfd891ec735abf4ff707d7d87d574107eefafa
Sergey Chernetsky <chernetsky@gmail.com>
2022-05-17 18:24:32 +0800

    Migration for view_tokens update

---
+++ sequelize/migrations/20220517101911-view_tokens-add-collection-owner.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.sequelize.query('DROP VIEW view_tokens;', { transaction });
+      await queryInterface.sequelize.query(
+        `
+        CREATE OR REPLACE VIEW public.view_tokens
+        AS SELECT t.token_id,
+            t.collection_id,
+            t.data,
+            t.owner,
+            t.owner_normalized,
+            COALESCE(t.data::json ->> 'ipfsJson'::text, replace(COALESCE(c.offchain_schema, ''::text), '{id}'::text, t.token_id::character varying(255)::text), ''::text) AS image_path,
+            c.token_prefix,
+            c.name AS collection_name,
+            c.description AS collection_description,
+            c.variable_on_chain_schema::json ->> 'collectionCover'::text AS collection_cover,
+            c.owner AS collection_owner,
+			      c.owner_normalized AS collection_owner_normalized,
+            t.date_of_creation
+          FROM tokens t
+            LEFT JOIN collections c ON c.collection_id = t.collection_id;
+        `,
+        { transaction },
+      );
+      await transaction.commit();
+    } catch (err) {
+      console.error(err);
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.sequelize.query('DROP VIEW view_tokens;', { transaction });
+      await queryInterface.sequelize.query(
+        `
+        CREATE OR REPLACE VIEW public.view_tokens
+        AS SELECT t.token_id,
+            t.collection_id,
+            t.data,
+            t.owner,
+            COALESCE(t.data::json ->> 'ipfsJson'::text, replace(COALESCE(c.offchain_schema, ''::text), '{id}'::text, t.token_id::character varying(255)::text), ''::text) AS image_path,
+            c.token_prefix,
+            c.name AS collection_name,
+            c.description AS collection_description,
+            c.variable_on_chain_schema::json ->> 'collectionCover'::text AS collection_cover,
+            t.date_of_creation
+          FROM tokens t
+            LEFT JOIN collections c ON c.collection_id = t.collection_id;
+        `,
+        { transaction },
+      );
+      await transaction.commit();
+    } catch (err) {
+      console.error(err);
+      await transaction.rollback();
+      throw err;
+    }
+  }
+};
